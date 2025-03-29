1d58d6d781a2c5b77bce413873e09cd65d979721
ChuyB <bovea3359@gmail.com>
2025-03-29 19:29:19 -0400

    Añade solución problema 3

---
+++ spark/3_tweets_violentos/problema3.py
@@ -0,0 +1,47 @@
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import col
+from pyspark.sql.functions import lower
+from pyspark.sql.functions import split
+from pyspark.sql.functions import collect_set
+from pyspark.sql.functions import struct
+from pyspark.sql.functions import size
+from pyspark.sql.functions import expr
+from pyspark.sql.functions import desc
+
+#spark = SparkSession.builder.remote("sc://localhost:15002").getOrCreate()
+spark = SparkSession.builder.appName("problema_3").getOrCreate()
+
+# Cargar el archivo JSON y las palabras
+df = spark.read.json("twitter/us_news_tweets.json")
+vio_keywords = spark.read.text("vio_keywords.txt")
+
+# Tomar lo que queremos de los tweets
+tweets = df.select("content","user.username","likeCount","retweetCount","place.country","place.fullName")
+
+# Clasificar por pais
+paises_tweets = tweets.filter(tweets["country"] == "United States")
+
+# Realizar el producto cruzado para luego ver si las palabras estan en los tweets
+producto_cruz = paises_tweets.crossJoin(vio_keywords)
+tweets_violentos = producto_cruz.filter(lower(col("content")).contains(col("value")))
+
+# Extraer el estado y eliminamos columnas innecesarias
+tweets_estados = tweets_violentos.withColumn("state", split(tweets_violentos["fullName"], ", ")[1])
+tweets_estados = tweets_estados.drop("country").drop("fullName").drop("value")
+
+# Agrupamos por estado sin eliminar los tweets del grupo y los contamos
+tweets_agrupados = tweets_estados.groupBy("state").agg(collect_set(struct("*")).alias("tweets"))
+num_tweets = tweets_agrupados.withColumn("cantidad_tweets", size("tweets"))
+
+# Para mostrar mejor los resultados, eliminamos los states de cada tweet (ya que ya esta en el grupo) y reorganizamos las columnas
+tweets_no_state = num_tweets.withColumn(
+    "tweets",
+    expr("transform(tweets, x -> struct(x.content as content, x.username as username, x.likeCount as likeCount, x.retweetCount as retweetCount))")
+)
+reordenamiento_cols = tweets_no_state.select("state", "cantidad_tweets", "tweets")
+
+# Ordenamos por cantidad_tweets
+tweets_ordenados = reordenamiento_cols.orderBy(desc("cantidad_tweets"))
+
+# Guardamos los resultados
+tweets_ordenados.write.json("salida_problema3_spark")
---
+++ spark/3_tweets_violentos/problema3sql.py
@@ -0,0 +1,83 @@
+from pyspark.sql import SparkSession
+
+SparkSession.builder.master("local[*]").getOrCreate().stop()
+
+spark = SparkSession.builder.remote("sc://localhost:15002").getOrCreate()
+
+# Cargar el archivo JSON y las palabras
+spark.sql("""
+CREATE OR REPLACE TEMP VIEW tweets AS
+SELECT content, user.username AS username, likeCount, retweetCount, 
+       place.country AS country, place.fullName AS fullName
+FROM json.`twitter/us_news_tweets.json`
+""")
+spark.sql("""
+CREATE OR REPLACE TEMP VIEW vio_keywords AS
+SELECT value
+FROM text.`vio_keywords.txt`
+""")
+
+# Mostrar los datos de la vista 'tweets'
+spark.sql("SELECT * FROM tweets").show(10)  # Muestra las primeras 10 filas sin truncar el contenido
+
+# Mostrar los datos de la vista 'vio_keywords'
+spark.sql("SELECT * FROM vio_keywords").show(10)  # Muestra las primeras 10 filas sin truncar el contenido
+
+# Filtrar por país
+spark.sql("""
+CREATE OR REPLACE TEMP VIEW paises_tweets AS
+SELECT *
+FROM tweets
+WHERE country = 'United States'
+""")
+spark.sql("SELECT * FROM paises_tweets").show(10) # Mostrar resultado
+
+# Realizar el producto cruzado y buscar palabras clave en los tweets
+spark.sql("""
+CREATE OR REPLACE TEMP VIEW producto_cruz AS
+SELECT *
+FROM paises_tweets
+CROSS JOIN vio_keywords
+""")
+
+spark.sql("""
+CREATE OR REPLACE TEMP VIEW tweets_violentos AS
+SELECT *
+FROM producto_cruz
+WHERE LOWER(content) LIKE CONCAT('%', value, '%')
+""")
+# Mostrar 10 primeros resultados
+spark.sql("SELECT * FROM producto_cruz").show(10)
+
+# Extraer el estado y eliminar columnas innecesarias
+spark.sql("""
+CREATE OR REPLACE TEMP VIEW tweets_estados AS
+SELECT SPLIT(fullName, ', ')[1] AS state, content, username, likeCount, retweetCount
+FROM tweets_violentos
+""")# Agrupar por estado y contar los tweets
+spark.sql("""
+CREATE OR REPLACE TEMP VIEW tweets_agrupados AS
+SELECT state, COLLECT_LIST(struct(content, username, likeCount, retweetCount)) AS tweets
+FROM tweets_estados
+GROUP BY state
+""")
+
+spark.sql("""
+CREATE OR REPLACE TEMP VIEW num_tweets AS
+SELECT state, SIZE(tweets) AS cantidad_tweets, tweets
+FROM tweets_agrupados
+""")
+
+# Reorganizar las columnas y ordenar los resultados
+resultados = spark.sql("""
+SELECT state, cantidad_tweets, 
+       TRANSFORM(tweets, x -> struct(x.content, x.username, x.likeCount, x.retweetCount)) AS tweets
+FROM num_tweets
+ORDER BY cantidad_tweets DESC
+""")
+
+# Mostrar los primeros 10 resultados
+resultados.show(10)
+
+# Guardar los resultados en un archivo JSON
+resultados.write.json("salida_problema3_spark_sql")
